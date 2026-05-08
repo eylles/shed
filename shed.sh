067b4428aa64a8921d49958386863966e31c116c
eylles <ed.ylles1997@gmail.com>
2026-05-08 12:15:05 -0600

    Use make_dir for XDG_RUNTIME_DIR
    Allows us to remove an if check tho

--- shed.sh
+++ shed.sh
@@ -57,10 +57,8 @@ if [ -z "${XDG_RUNTIME_DIR}" ]; then
   else
     # welp, gotta define it as something
     export XDG_RUNTIME_DIR=/tmp/"${UserID}"-runtime-dir
-    if ! test -d "${XDG_RUNTIME_DIR}"; then
-      mkdir "${XDG_RUNTIME_DIR}"
-      chmod 0700 "${XDG_RUNTIME_DIR}"
-    fi
+    make_dir "${XDG_RUNTIME_DIR}"
+    chmod 0700 "${XDG_RUNTIME_DIR}"
   fi
 fi
 
