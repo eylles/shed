62680c104ebc70b64ee19d278d5f122d40a33d7c
eylles <ed.ylles1997@gmail.com>
2026-05-14 23:38:41 -0600

    Bump VERSION

--- version.mk
+++ version.mk
@@ -5,6 +5,6 @@
 # version determined by git
 GITTAG = $(shell git describe --tags 2>/dev/null)
 # last release tag
-VERS = v0.2.0
+VERS = v0.3.0
 # actual version number that will be used
 VERSION = $(if $(GITTAG),$(GITTAG),$(VERS))
